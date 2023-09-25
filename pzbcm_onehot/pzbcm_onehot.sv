9e0b6f3e3b0ef75525606b3857c3cc09d61a968b
Taichi Ishitani <ishitani@pezy.co.jp>
2023-09-25 10:23:07 +0900

    simplify implementation

--- pzbcm_onehot/pzbcm_onehot.sv
+++ pzbcm_onehot/pzbcm_onehot.sv
@@ -7,96 +7,61 @@
 interface pzbcm_onehot #(
   parameter int N = 1
 );
-  localparam  int INDEX_WIDTH   = $clog2(N + 1);
   localparam  int BINARY_WIDTH  = (N >= 2) ? $clog2(N) : 1;
 
   function automatic logic [N-1:0] __onehot(
-    logic [N-1:0]           bits,
-    logic [INDEX_WIDTH-1:0] n
+    int           n,
+    int           step,
+    logic [N-1:0] bits,
+    logic [N-1:0] bits_reduced
   );
-    if (n > 2) begin
-      logic [INDEX_WIDTH-1:0] half_n;
-      logic [N-1:0]           half_result[2];
+    int           next_n;
+    logic [N-1:0] next_bits;
+    logic [N-1:0] next_bits_reduced;
 
-      half_n  = INDEX_WIDTH'(n[INDEX_WIDTH-1:1]);
-      for (int i = 0;i < 2;++i) begin
-        logic [N-1:0]           next_bits;
-        logic [INDEX_WIDTH-1:0] next_n;
+    next_n  = (n / 2) + (n % 2);
+    for (int i = 0;i < next_n;++i) begin
+      logic [1:0] or_bits;
+      logic [1:0] or_result;
 
-        next_n  = ((i == 1) && n[0]) ? half_n + INDEX_WIDTH'(1) : half_n;
-        for (int j = 0;j < next_n;++j) begin
-          if (i == 0) begin
-            next_bits[j]  = bits[j];
-          end
-          else begin
-            next_bits[j]  = bits[j+half_n];
-          end
+      if (((i + 1) == next_n) && ((n % 2) == 1)) begin
+        or_bits = {1'b0, bits_reduced[2*i]};
+      end
+      else begin
+        or_bits = bits_reduced[2*i+:2];
+      end
+
+      if (or_bits[0]) begin
+        or_result = 2'b01;
+      end
+      else begin
+        or_result = 2'b10;
+      end
+
+      next_bits_reduced[i]  = |or_bits;
+      for (int j = 0;j < (2 * step);++j) begin
+        int index = (2 * step * i) + j;
+        if (index < N) begin
+          next_bits[index]  = or_result[j/step] && bits[index];
         end
-
-        half_result[i]  = __onehot(next_bits, next_n);
       end
-
-      return marge_result(half_result, n, half_n);
     end
-    else if (n == 2) begin
-      logic [N-1:0] result;
 
-      result  = bits;
-      if (bits[1:0] != 2'b10) begin
-        result[1] = '0;
-      end
+    if (next_n == 1) begin
+      return next_bits;
+    end
+    else begin
+      return __onehot(next_n, 2 * step, next_bits, next_bits_reduced);
+    end
+  endfunction
 
-      return result;
+  function automatic logic [N-1:0] to_onehot(logic [N-1:0] bits);
+    if (N > 1) begin
+      return __onehot(N, 1, bits, bits);
     end
     else begin
       return bits;
     end
-  endfunction
-
-  function automatic logic [N-1:0] marge_result(
-    logic [N-1:0]           result[2],
-    logic [INDEX_WIDTH-1:0] n,
-    logic [INDEX_WIDTH-1:0] half_n
-  );
-    logic [1:0]   ored;
-    logic [N-1:0] marged_result;
-
-    ored  = '0;
-    for (int i = 0;i < n;++i) begin
-      if (i < half_n) begin
-        ored[0] |= result[0][i];
-      end
-      else begin
-        ored[1] |= result[1][i-half_n];
-      end
-    end
-
-    if (ored == 2'b10) begin
-      for (int i = 0;i < n;++i) begin
-        if (i < half_n) begin
-          marged_result[i]  = '0;
-        end
-        else begin
-          marged_result[i]  = result[1][i-half_n];
-        end
-      end
-    end
-    else begin
-      for (int i = 0;i < n;++i) begin
-        if (i < half_n) begin
-          marged_result[i]  = result[0][i];
-        end
-        else begin
-          marged_result[i]  = '0;
-        end
-      end
-    end
-
-    return marged_result;
-  endfunction
-
-  function automatic logic [N-1:0] to_onehot(logic [N-1:0] bits);
-    return __onehot(bits, INDEX_WIDTH'(N));
   endfunction
 
   function automatic logic [BINARY_WIDTH-1:0] to_binary(logic [N-1:0] bits);
