1b9612b25de5af028e4657af8dd38e581e1af60d
Taichi Ishitani <ishitani@pezy.co.jp>
2023-12-15 15:25:12 +0900

    add include directory

--- pzcorebus_pkg.list.rb
+++ pzcorebus_pkg.list.rb
@@ -4,7 +4,9 @@
 ##                    All Rights Reserved.
 ##
 ##========================================
+include_directory 'pzcorebus_common'
 source_file 'pzcorebus_common/pzcorebus_pkg.sv'
+
 find_files 'pzcorebus_*/*_pkg.sv', from: :current do |pkg|
   source_file pkg
 end
