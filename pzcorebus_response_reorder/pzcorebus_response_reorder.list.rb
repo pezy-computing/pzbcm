ca59449825e621bc0ad2e70ea11876bdff6d33b3
ishitani <ishitani@pezy.co.jp>
2024-03-11 09:44:45 +0900

    add response reorder

---
+++ pzcorebus_response_reorder/pzcorebus_response_reorder.list.rb
@@ -0,0 +1,9 @@
+##========================================
+##
+## Copyright (c) 2024 PEZY Computing, K.K.
+##                    All Rights Reserved.
+##
+##========================================
+file_list   'pzcorebus_common/pzcorebus_common.list.rb'
+file_list   'pzcorebus_slicer/pzcorebus_slicer.list.rb'
+source_file 'pzcorebus_response_reorder.sv'
